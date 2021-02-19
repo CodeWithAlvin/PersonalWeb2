f12cd71973436d21d9c59bfcdaf985b41c8e0c35
alvin saini <codewithalvin@gmail.com>
2021-02-19 15:36:41 +0530

    added model for skills

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from mainapp.models import Skill
 
 # Create your views here.
 def index(reuests):
-    skillist={"python":90,"html":85,"Javascript":30}
+    skilllist={}
+    for i in Skill.objects.values():
+        skilllist['']
     return render(reuests,"index.html",context={"skillist":skillist})
-
